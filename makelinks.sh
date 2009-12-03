447e31290ffd2d3a73263ed61b3bceeb1d44f506
mjhooker <mjhooker@mjhooker-desktop.(none)>
2009-12-03 02:14:48 +0000

    made wget less verbose -nv

--- makelinks.sh
+++ makelinks.sh
@@ -8,7 +8,7 @@ for i in jaunty jaunty-security jaunty-updates
    do
 
 
-wget -m http://archive.ubuntu.com/ubuntu/dists/${i}/${j}/binary-i386/Packages.bz2 
+wget -nv -m http://archive.ubuntu.com/ubuntu/dists/${i}/${j}/binary-i386/Packages.bz2 
 bzip2 -d < archive.ubuntu.com/ubuntu/dists/${i}/${j}/binary-i386/Packages.bz2 > archive.ubuntu.com/ubuntu/dists/${i}/${j}/binary-i386/Packages
 cat archive.ubuntu.com/ubuntu/dists/${i}/${j}/binary-i386/Packages >> allpackages
 

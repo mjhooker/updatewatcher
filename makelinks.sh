f6ce497999070925c570141485526fd759214fde
mjhooker <mjhooker@mjhooker-netbook.(none)>
2010-03-09 19:51:05 +0000

    factor out archive site to variable

--- makelinks.sh
+++ makelinks.sh
@@ -1,6 +1,8 @@
 #/bin/bash
 
 OPT=-nv
+HOST=archive.ubuntu.com
+
 
 rm allpackagefiles
 
@@ -9,15 +11,15 @@ for i in jaunty jaunty-security jaunty-updates
   for j in main multiverse universe restricted 
    do
 
-
-wget $OPT -m http://archive.ubuntu.com/ubuntu/dists/${i}/${j}/binary-i386/Packages.bz2 
-if [ archive.ubuntu.com/ubuntu/dists/${i}/${j}/binary-i386/Packages -nt archive.ubuntu.com/ubuntu/dists/${i}/${j}/binary-i386/Packages ]
+    LOC=${HOST}/ubuntu/dists/${i}/${j}/binary-i386/Packages
+wget $OPT -m http://${LOC}.bz2 
+if [ ${LOC}.bz2 -nt ${LOC} ]
 then
  echo unzippping
- bzip2 -d < archive.ubuntu.com/ubuntu/dists/${i}/${j}/binary-i386/Packages.bz2 > archive.ubuntu.com/ubuntu/dists/${i}/${j}/binary-i386/Packages
+ bzip2 -d < ${LOC}.bz2 > ${LOC}
 fi
 
-echo archive.ubuntu.com/ubuntu/dists/${i}/${j}/binary-i386/Packages >> allpackagefiles
+echo ${LOC} >> allpackagefiles
 
     echo updated $j $i
    done

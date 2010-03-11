dff1b8b53e7d29836ce8aa9f75356e712ec4f301
mjhooker <mjhooker@mjhooker-desktop.(none)>
2010-03-11 23:58:02 +0000

    store guid, modify excludes

--- getpackages.sh
+++ getpackages.sh
@@ -4,3 +4,9 @@
 echo checking installed packages
 dpkg-query -W -f '${Status;1}\t${Package}\t${Version}\n' | grep -v "^d" > packages
 gzip < packages > packages.gz
+
+if [ ! -a system.inf ]
+then
+ echo generating guid
+ uuidgen > system.inf
+fi

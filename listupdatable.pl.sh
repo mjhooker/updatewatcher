090ae9d3c1af55790693f2051f1d6002046f5314
mjhooker <mjhooker@novatech.(none)>
2010-07-07 10:09:14 +0100

    add code to check for site.inf

--- listupdatable.pl.sh
+++ listupdatable.pl.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+
+if [ -e site.inf ]
+then
+
 ./makelinks.sh
 ./getpackages.sh
 ./readpkg.pl
 POST http://`cat site.inf`/cgi-bin/updateable.pl?guid=`cat system.inf` < updatedpackages.html
+
+
+else
+ echo please set site name in site.inf
+fi
+
 

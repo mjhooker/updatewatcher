84a547067611f9e3a675be16c1dda2626bc14e7f
mjhooker <mjhooker@mjhooker-laptop.(none)>
2010-09-05 01:25:33 +0100

    preparse the packages files - cuts down time taken to recompute updates by about 25%

--- readpkg.pl
+++ readpkg.pl
@@ -28,36 +28,8 @@ open A,"cat `cat allpackagefiles` |";
 
 while ($ipline=<A>)
 {
-#  print $ipline;
-
-  $want="Package";
-  if (substr($ipline,0,length($want)) eq $want)
-  {
-    $foundpackage=substr($ipline,length($want)+2,-1);
-  }
-
-  $want="Version";
-  if (substr($ipline,0,length($want)) eq $want)
-  {
-    $foundversion=substr($ipline,length($want)+2,-1);
-#    print "Package [".$foundpackage."]\n";
-#    print $want." [".$foundversion."]\n";
-    if (exists $latestp{$foundpackage})
-    {
-      if ($latestp{$foundpackage} ne $foundversion)
-      {
-        $compare=system("dpkg --compare-versions ".$latestp{$foundpackage}." lt ".$foundversion);
-        if ($compare==0)
-        {
-#          print "replacing ".$latestp{$foundpackage}." with ".$foundversion."\n";
-          $latestp{$foundpackage}=$foundversion;
-        }
-      }
-    } else {
-        $latestp{$foundpackage}=$foundversion;
-    }
-  }
-
+  @fields=split(/\t/,$ipline);
+  $latestp{$fields[0]}=$fields[1];
 }
 
 close A;

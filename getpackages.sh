c7999f9233107c960ccb06fc2d9721d7ea866669
mjhooker <mjhooker@dell-server.(none)>
2010-09-13 22:38:08 +0100

    make it simpler changes to client end

--- getpackages.sh
+++ getpackages.sh
@@ -1,28 +1,17 @@
 #!/bin/bash
 
-# dpkg-query -l | grep -v "^rc" > packages
-# echo checking installed packages
 dpkg-query -W -f '${Status;1}\t${Package}\t${Version}\n' | grep -v "^d" > packages
-#gzip < packages > packages.gz
 
 OLD=`cat packages.md5`
 NEW=`md5sum packages`
 echo "${NEW}" > packages.md5
 
-if [ -e packages.md5 ]
-then
-
 
 
  if [[ ! ${NEW} == ${OLD} ]]
  then
-  echo installed packages updated
+ # installed packages updated
 
   POST -d http://`cat site.inf`/cgi-bin/packages.pl?guid=`cat system.inf` < packages
 
-# else
-#  echo no change to installed packages
  fi
-fi
-
-
--- makelinks.sh
+++ makelinks.sh
@@ -1,17 +1,6 @@
 #/bin/bash
 
-#OPT=-nv
-#HOST=archive.ubuntu.com
-
-
-rm allpackagefiles
-
-for i in `./getrepo.pl` 
+( for i in `./getrepo.pl` 
  do
-    LOC=${i}
-    echo ${LOC} >> allpackagefiles
-
-#   done
- done
-
-POST -d http://`cat site.inf`/cgi-bin/repository.pl?guid=`cat system.inf` < allpackagefiles
+    echo ${i} 
+ done ) | POST -d http://`cat site.inf`/cgi-bin/repository.pl?guid=`cat system.inf`

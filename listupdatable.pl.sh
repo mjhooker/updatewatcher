419ea2c467efccfbb7d2277b4cc02f4d7ad7c8aa
mjhooker <mjhooker@mjhooker-laptop.(none)>
2010-09-17 01:30:23 +0100

    changed listupdateable.pl.sh to include siteinf.sh

--- listupdatable.pl.sh
+++ listupdatable.pl.sh
@@ -1,24 +1,9 @@
 #!/bin/bash
 
-if [ ! -e system.inf ]
-then
- echo generating guid
- uuidgen > system.inf
-fi
+source siteinf.sh
 
-
-
-if [ -e site.inf ]
-then
-
-if [ -e machine.inf ]
-then
-
-if [ -e account.inf ]
-then
-
-ADDR=http://`cat site.inf`/cgi-bin/
-GUID=\?guid=`cat system.inf`
+echo ${MACH} | POST -d ${ADDR}machine.pl${GUID}
+echo ${ACC} | POST -d ${ADDR}account.pl${GUID}
 
 
 # ./makelinks.sh
@@ -30,42 +15,5 @@ GUID=\?guid=`cat system.inf`
 
 # ./getpackages.sh
 
-dpkg-query -W -f '${Status;1}\t${Package}\t${Version}\n' | grep -v "^d" > packages
-
-OLD=`cat packages.md5`
-NEW=`md5sum packages`
-echo "${NEW}" > packages.md5
-
-
-
- if [[ ! ${NEW} == ${OLD} ]]
- then
- # installed packages updated
-
-  POST -d ${ADDR}packages.pl${GUID} < packages
-
- fi
-
-
-POST -d ${ADDR}machine.pl${GUID} < machine.inf
-POST -d ${ADDR}account.pl${GUID} < account.inf
-
-
-
-else
- echo please provide account email address in account.inf
-fi
-
-
-
-
-else
- echo please set machine descriptor in machine.inf
-fi
-
-
-
-else
- echo please set site name in site.inf
-fi
+dpkg-query -W -f '${Status;1}\t${Package}\t${Version}\n' | grep -v "^d" | POST -d ${ADDR}packages.pl${GUID}
 

908a3c8e694986b8ba755e8f05164fb84c7f4003
mjhooker <mjhooker@mjhooker-laptop.(none)>
2011-03-07 02:39:17 +0000

    added code to generate siteinf.sh

--- listupdatable.pl.sh
+++ listupdatable.pl.sh
@@ -26,4 +26,16 @@ dpkg-query -W -f '${Status;1}\t${Package}\t${Version}\n' | grep -v "^d" | POST -
 
 else
  echo needs $CONFIG file
+ read -p "enter site name:" -e SITE
+ ADDR=https://${SITE}/cgi-bin
+ MGUID=`GET ${ADDR}/newguid.pl`
+ echo guid is $MGUID
+ read -p "enter machine name:" -e MACH
+ read -p "enter account email address:" -e ACC
+
+ echo "MGUID=$MGUID" > siteinf.sh
+ echo "ADDR=${ADDR}" >> siteinf.sh
+ echo "MACH=\"$MACH\"" >> siteinf.sh
+ echo "ACC=${ACC}" >> siteinf.sh 
+
 fi
